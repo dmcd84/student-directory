1c138c9d0731ba22465eb45cac5ecba53fc4f0ab
Dom <dom.mcdonnell@gmail.com>
2017-04-28 08:47:29 +0100

    Print numbered student list

--- directory.rb
+++ directory.rb
@@ -1,11 +1,11 @@
 def input_students
   puts "Please enter the names of the students"
   puts "To finish, just hit return twice"
-  # creates an empty array
+  # create an empty array
   students = []
   # get the first name
   name = gets.chomp
-  # while the name is not empty repeat this code
+  # while the name is not empty, repeat this code
   while !name.empty? do
     # add the student hash to the array
     students << {name: name, cohort: :november}
@@ -23,8 +23,8 @@ def print_header
 end
 
 def print(students)
-  students.each do |student|
-    puts "#{student[:name]} (#{student[:cohort]} cohort)"
+  students.each_with_index do |student, index|
+    puts "#{index+=1}. #{student[:name]} (#{student[:cohort]} cohort)"
   end
 end
 
