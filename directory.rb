4f80411a6713a419984e4c7dcd5fded27a99e705
Dom <dom.mcdonnell@gmail.com>
2017-04-28 12:35:52 +0100

    8.5 Student input accepts additional student details

--- directory.rb
+++ directory.rb
@@ -1,15 +1,26 @@
 def input_students
-  puts "Please enter the names of the students"
-  puts "To finish, just hit return twice"
+  puts "Please enter the details of the students"
+  puts "To finish, just hit return twice when presented with new name prompt"
   # create an empty array
   students = []
+  puts "Please enter student name"
   # get the first name
   name = gets.chomp
   # while the name is not empty, repeat this code
   while !name.empty? do
+    puts "Plesae indicate cohort for student"
+    cohort = gets.chomp
+    puts "Please enter main hobby of student"
+    hobby = gets.chomp
+    puts "Please enter country of birth for student"
+    country = gets.chomp
+    puts "Please enter heigh of student in cm"
+    height = gets.chomp
     # add the student hash to the array
-    students << {name: name, cohort: :november}
+    students << {name: name, cohort: cohort, hobbies: hobby, country_of_birth: country, height: height}
+    # this is where the new user input for student details needs to go.
     puts "Now we have #{students.count} students"
+    puts "Please enter student name"
     # get another name from the user
     name = gets.chomp
   end
@@ -25,7 +36,7 @@ end
 def print(students)
   x = 1
   while x <= students.count do
-    puts "#{x} #{students[x-1][:name]} (#{students[x-1][:cohort]} cohort)"
+    puts "#{x} #{students[x-1][:name]} #{students[x-1][:cohort]} #{students[x-1][:hobbies]} #{students[x-1][:country_of_birth]} #{students[x-1][:height]}cm"
     x += 1
   end
   puts "-- end of list --"
